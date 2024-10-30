a9e6fe27d0ae7f1410bedc3c0ca0633ab77a7de8
Cameron Meile <cameronmeile@edu.tbz.ch>
2024-10-30 08:46:09 +0000

    🔒️ auto: updated files

--- obj/Debug/net9.0/M223HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/M223HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("M223HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f702a2a905432edfa03abcc8ddb60e39959747f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc5b2c5857ff811a4785ee16ccd53e844d692f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("M223HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("M223HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
